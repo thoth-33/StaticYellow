dc59c98cd9327618bd7c1e9b691c108c32c703e0
JA2398 <jahern2398@gmail.com>
2024-12-23 16:14:27 -0600

    Traded Pokemon Evolutions
    4 Pokemon: Can now evolve either by trade or level up.

--- data/pokemon/evos_moves.asm
+++ data/pokemon/evos_moves.asm
@@ -629,6 +629,7 @@ SlowpokeEvosMoves:
 KadabraEvosMoves:
 ; Evolutions
 	db EVOLVE_TRADE, 1, ALAKAZAM
+	db EVOLVE_LEVEL, 44, ALAKAZAM
 	db 0
 ; Learnset
 	db 16, CONFUSION
@@ -642,6 +643,7 @@ KadabraEvosMoves:
 GravelerEvosMoves:
 ; Evolutions
 	db EVOLVE_TRADE, 1, GOLEM
+	db EVOLVE_LEVEL, 44, GOLEM
 	db 0
 ; Learnset
 	db 11, DEFENSE_CURL
@@ -668,6 +670,7 @@ ChanseyEvosMoves:
 MachokeEvosMoves:
 ; Evolutions
 	db EVOLVE_TRADE, 1, MACHAMP
+	db EVOLVE_LEVEL, 44, MACHAMP
 	db 0
 ; Learnset
 	db 20, LOW_KICK
@@ -1721,6 +1724,7 @@ MissingNo92EvosMoves:
 HaunterEvosMoves:
 ; Evolutions
 	db EVOLVE_TRADE, 1, GENGAR
+	db EVOLVE_LEVEL, 44, GENGAR
 	db 0
 ; Learnset
 	db 29, HYPNOSIS
