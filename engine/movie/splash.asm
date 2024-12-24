20dec32f9db32b01c3341b8a91ac55f7fcf9daf0
CreamElDudJafar <jahern2398@gmail.com>
2024-12-24 14:16:13 -0600

    The splash screen adds 2 more stars than it should
    The splash screen appears to add 2 extra yet invisible stars during the shooting star animation

--- engine/movie/splash.asm
+++ engine/movie/splash.asm
@@ -136,7 +136,7 @@ AnimateShootingStar:
 	ld a, [wMoveDownSmallStarsOAMCount]
 	cp 24
 	jr z, .next2
-	add 6 ; should be 4, but the extra 2 aren't visible on screen
+	add 4
 	ld [wMoveDownSmallStarsOAMCount], a
 .next2
 	call MoveDownSmallStars
