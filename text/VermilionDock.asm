78ac3d56fe5ff14e233dcedfd8028223ab4ce6db
JA2398 <jahern2398@gmail.com>
2025-01-03 10:03:06 -0600

    Vermilion Dock Rock Text
    Rock Under The Truck

--- text/VermilionDock.asm
+++ text/VermilionDock.asm
@@ -1,3 +1,13 @@
 _VermilionDockUnusedText::
 	text_start
 	done
+
+_VermilionDockTruckText::
+	text "Something is"
+	line "under this truck."
+
+	para "I should take a"
+	line "look."
+
+	para "It's just a rock!"
+	done
