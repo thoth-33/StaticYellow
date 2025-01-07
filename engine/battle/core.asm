647123dd400e120b97cb6aceec3335082507786c
JA2398 <jahern2398@gmail.com>
2025-01-07 11:46:06 -0600

    Elite 4 Members Play Gym Leader Music
    Pokered Wiki: Thanks YakiNeed and everyone involved.

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -937,8 +937,19 @@ TrainerBattleVictory:
 .gymleader
 	ld a, [wTrainerClass]
 	cp RIVAL3 ; final battle against rival
-	jr nz, .notrival
+	jr z, .special1998
+	cp LORELEI
+	jr z, .special1998
+	cp BRUNO
+	jr z, .special1998
+	cp AGATHA
+	jr z, .special1998
+	cp LANCE
+	jr nz, .special1998
+.special1998	
 	ld b, MUSIC_DEFEATED_GYM_LEADER
+	cp RIVAL3 ; final battle against rival
+	jr nz, .notrival
 	ld hl, wStatusFlags7
 	set BIT_NO_MAP_MUSIC, [hl]
 .notrival
