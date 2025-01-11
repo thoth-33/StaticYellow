36a92e5643af8053d2756e199b4562913f0d57c4
JA2398 <jahern2398@gmail.com>
2025-01-11 12:46:53 -0600

    Removed Unused Names
    Not used in English Yellow. Commented out.

--- main.asm
+++ main.asm
@@ -6,7 +6,7 @@ INCLUDE "engine/movie/title.asm"
 INCLUDE "engine/pokemon/load_mon_data.asm"
 INCLUDE "data/items/prices.asm"
 INCLUDE "data/items/names.asm"
-INCLUDE "data/text/unused_names.asm"
+;INCLUDE "data/text/unused_names.asm" ; Removed from game
 INCLUDE "engine/gfx/sprite_oam.asm"
 INCLUDE "engine/link/print_waiting_text.asm"
 INCLUDE "engine/overworld/sprite_collisions.asm"
