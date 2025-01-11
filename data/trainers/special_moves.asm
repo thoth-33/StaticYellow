a62914e4a19b5a484b7663f7dbb3ec22ada0474d
JA2398 <jahern2398@gmail.com>
2025-01-11 11:45:32 -0600

    Special Moves for Trainers
    All Gym Leaders and E4 Done. Rival needs to be done still.

--- data/trainers/special_moves.asm
+++ data/trainers/special_moves.asm
@@ -14,147 +14,286 @@ SpecialTrainerMoves:
 	db 0
 
 	db BROCK, 1
-	db 2, 3, BIND
-	db 2, 4, BIDE
+	db 1, 1, TACKLE
+	db 1, 2, DEFENSE_CURL
+	db 1, 3, ROCK_THROW
+	db 2, 1, SUPERSONIC
+	db 2, 2, GUST
+	db 2, 3, CONFUSE_RAY
+	db 3, 1, SCREECH
+	db 3, 2, BIDE
+	db 3, 3, BIND
 	db 0
 
 	db MISTY, 1
-	db 2, 4, BUBBLEBEAM
+	db 1, 1, DISABLE
+	db 1, 2, CONFUSION
+	db 1, 3, RAGE
+	db 2, 1, TOXIC
+	db 2, 2, WATER_GUN
+	db 2, 3, REFLECT
+	db 3, 1, DOUBLE_TEAM
+	db 3, 2, THUNDER_WAVE
+	db 3, 3, BUBBLEBEAM
 	db 0
 
 	db LT_SURGE, 1
-	db 1, 1, THUNDERBOLT
-	db 1, 2, MEGA_PUNCH
-	db 1, 3, MEGA_KICK
-	db 1, 4, GROWL
+	db 1, 1, RAGE
+	db 1, 2, THUNDERPUNCH
+	db 1, 3, SCREECH
+	db 2, 1, DOUBLE_TEAM
+	db 2, 2, REFLECT
+	db 2, 3, SWIFT
+	db 3, 1, AGILITY
+	db 3, 2, QUICK_ATTACK
+	db 3, 3, THUNDERSHOCK
+	db 4, 1, THUNDER
+	db 4, 2, THUNDER_WAVE
+	db 4, 3, SURF
 	db 0
 
 	db ERIKA, 1
-	db 1, 3, MEGA_DRAIN
-	db 2, 1, RAZOR_LEAF
-	db 3, 1, PETAL_DANCE
+	db 1, 1, MEGA_DRAIN
+	db 1, 2, REFLECT
+	db 1, 3, POISONPOWDER
+	db 2, 1, ACID
+	db 2, 2, STUN_SPORE
+	db 2, 3, SLUDGE
+	db 3, 1, SLEEP_POWDER
+	db 3, 2, ABSORB
+	db 3, 3, VINE_WHIP
+	db 4, 1, PETAL_DANCE
+	db 4, 2, CUT
+	db 4, 3, MEGA_DRAIN
 	db 0
 
 	db KOGA, 1
 	db 1, 1, TOXIC
-	db 1, 2, TACKLE
-	db 2, 1, TOXIC
-	db 2, 3, SUPERSONIC
-	db 3, 1, TOXIC
-	db 3, 2, DOUBLE_EDGE
-	db 4, 1, LEECH_LIFE
-	db 4, 2, DOUBLE_TEAM
-	db 4, 3, PSYCHIC_M
-	db 4, 4, TOXIC
+	db 1, 2, DOUBLE_TEAM
+	db 1, 3, CONFUSE_RAY
+	db 2, 1, SLUDGE
+	db 2, 2, MEGA_DRAIN
+	db 2, 3, THUNDERBOLT
+	db 3, 1, FIRE_BLAST
+	db 3, 2, AMNESIA
+	db 3, 3, EXPLOSION
+	db 4, 1, DOUBLE_TEAM
+	db 4, 2, PSYCHIC_M
+	db 4, 3, CONFUSION
 	db 0
 
-	db BLAINE, 1
-	db 1, 1, FLAMETHROWER
-	db 1, 4, CONFUSE_RAY
-	db 3, 1, FLAMETHROWER
-	db 3, 2, FIRE_BLAST
-	db 3, 3, REFLECT
+	db BLAINE, 1	
+	db 1, 1, FIRE_SPIN
+	db 1, 2, TAKE_DOWN
+	db 1, 3, SWIFT
+	db 2, 1, DIG
+	db 2, 2, NIGHT_SHADE
+	db 2, 3, REFLECT
+	db 3, 1, BODY_SLAM
+	db 3, 2, HORN_DRILL
+	db 3, 3, FIRE_BLAST
+	db 4, 1, FLAMETHROWER
+	db 4, 2, AGILITY
+	db 4, 3, DIG
+	db 5, 1, FIRE_BLAST
+	db 5, 2, CONFUSE_RAY
+	db 5, 3, PSYCHIC_M
 	db 0
 
 	db SABRINA, 1
-	db 1, 1, FLASH
-	db 2, 1, KINESIS
-	db 2, 4, PSYWAVE
-	db 3, 1, PSYWAVE
+	db 1, 1, PSYCHIC_M
+	db 1, 2, SURF
+	db 1, 3, THUNDER_WAVE
+	db 2, 1, MEGA_KICK
+	db 2, 2, BARRIER
+	db 2, 3, SOLARBEAM
+	db 3, 1, ICE_BEAM
+	db 3, 2, COUNTER
+	db 3, 3, METRONOME
+	db 4, 1, HYPNOSIS
+	db 4, 2, MEGA_PUNCH
+	db 4, 3, PSYCHIC_M
+	db 5, 1, THUNDER_WAVE
+	db 5, 2, REFLECT
+	db 5, 3, DIG
 	db 0
 
+	db GIOVANNI, 1
+	db 1, 1, ROCK_THROW
+	db 1, 2, DIG
+	db 1, 3, SCREECH
+	db 2, 1, RAGE
+	db 2, 2, FIRE_BLAST
+	db 2, 3, HORN_DRILL
+	db 3, 1, MEGA_PUNCH
+	db 3, 2, SUBMISSION
+	db 3, 3, DIZZY_PUNCH
+	db 4, 1, TAKE_DOWN
+	db 4, 2, THUNDERBOLT
+	db 4, 3, BUBBLEBEAM
+	db 0
+
+	
+	db GIOVANNI, 2
+	db 1, 1, HARDEN
+	db 1, 2, CRABHAMMER
+	db 1, 3, GUILLOTINE
+	db 2, 1, MEGA_PUNCH
+	db 2, 2, SUBMISSION
+	db 2, 3, DIZZY_PUNCH
+	db 3, 1, TOXIC
+	db 3, 2, COUNTER
+	db 3, 3, EARTHQUAKE
+	db 4, 1, DOUBLE_TEAM
+	db 4, 2, SCREECH
+	db 4, 3, EARTHQUAKE
+	db 5, 1, THUNDERBOLT
+	db 5, 2, ICE_BEAM
+	db 5, 3, FIRE_BLAST
+	db 0
+
+	
 	db GIOVANNI, 3
-	db 1, 3, FISSURE
-	db 2, 2, DOUBLE_TEAM
-	db 3, 1, EARTHQUAKE
-	db 3, 3, THUNDER
-	db 4, 1, EARTHQUAKE
-	db 4, 2, LEER
-	db 4, 3, THUNDER
-	db 5, 1, ROCK_SLIDE
-	db 5, 4, EARTHQUAKE
+	db 1, 1, EARTHQUAKE
+	db 1, 2, SELFDESTRUCT
+	db 1, 3, EXPLOSION
+	db 2, 1, SLASH
+	db 2, 2, THUNDERBOLT
+	db 2, 3, HYPER_BEAM
+	db 3, 1, DOUBLE_EDGE
+	db 3, 2, HYPER_BEAM
+	db 3, 3, BODY_SLAM
+	db 4, 1, BLIZZARD
+	db 4, 2, THUNDER
+	db 4, 3, SLUDGE
+	db 5, 1, ICE_BEAM
+	db 5, 2, THUNDER
+	db 5, 3, BODY_SLAM
+	db 6, 1, SUBMISSION
+	db 6, 2, THUNDERBOLT
+	db 6, 3, ROCK_SLIDE
 	db 0
 
+	
 	db LORELEI, 1
-	db 1, 1, BUBBLEBEAM
-	db 2, 3, ICE_BEAM
-	db 3, 1, PSYCHIC_M
-	db 3, 2, SURF
-	db 4, 3, LOVELY_KISS
-	db 5, 3, BLIZZARD
+	db 1, 1, SURF
+	db 1, 2, REST
+	db 1, 3, ICE_BEAM
+	db 2, 1, THUNDER_WAVE
+	db 2, 2, HYDRO_PUMP
+	db 2, 3, PSYCHIC_M
+	db 3, 1, EXPLOSION
+	db 3, 2, BLIZZARD
+	db 3, 3, SPIKE_CANNON
+	db 4, 1, AMNESIA
+	db 4, 2, PSYCHIC_M
+	db 4, 3, EARTHQUAKE
+	db 5, 1, LOVELY_KISS
+	db 5, 2, BODY_SLAM
+	db 5, 3, PSYCHIC_M
+	db 6, 1, SING
+	db 6, 2, THUNDERBOLT
+	db 6, 3, BLIZZARD
 	db 0
 
+	
 	db BRUNO, 1
 	db 1, 1, ROCK_SLIDE
-	db 1, 2, SCREECH
-	db 1, 4, DIG
-	db 2, 3, FIRE_PUNCH
-	db 2, 4, DOUBLE_TEAM
-	db 3, 1, DOUBLE_KICK
-	db 3, 2, MEGA_KICK
-	db 3, 4, DOUBLE_TEAM
-	db 4, 1, ROCK_SLIDE
-	db 4, 2, SCREECH
-	db 4, 4, EARTHQUAKE
-	db 5, 2, KARATE_CHOP
-	db 5, 3, STRENGTH
+	db 1, 2, EXPLOSION
+	db 1, 3, EARTHQUAKE
+	db 2, 1, FISSURE
+	db 2, 2, TAKE_DOWN
+	db 2, 3, EXPLOSION
+	db 3, 1, ICE_PUNCH
+	db 3, 2, FIRE_PUNCH
+	db 3, 3, THUNDERPUNCH
+	db 4, 1, HI_JUMP_KICK
+	db 4, 2, MEGA_KICK
+	db 4, 3, STRENGTH
+	db 5, 1, HYPNOSIS
+	db 5, 2, AMNESIA
+	db 5, 3, ICE_BEAM
+	db 6, 1, EARTHQUAKE
+	db 6, 2, KARATE_CHOP
+	db 6, 3, HYPER_BEAM
 	db 0
 
+	
 	db AGATHA, 1
-	db 1, 2, SUBSTITUTE
-	db 1, 3, LICK
-	db 1, 4, MEGA_DRAIN
-	db 2, 2, TOXIC
-	db 2, 4, LEECH_LIFE
-	db 3, 2, LICK
-	db 4, 1, WRAP
-	db 5, 2, PSYCHIC_M
+	db 1, 1, FIRE_BLAST
+	db 1, 2, MINIMIZE
+	db 1, 3, BODY_SLAM
+	db 2, 1, SWORDS_DANCE
+	db 2, 2, WRAP
+	db 2, 3, SURF
+	db 3, 1, SLEEP_POWDER
+	db 3, 2, DOUBLE_EDGE
+	db 3, 3, LEECH_SEED
+	db 4, 1, HYPNOSIS
+	db 4, 2, DREAM_EATER
+	db 4, 3, PSYWAVE
+	db 5, 1, GLARE
+	db 5, 2, WRAP
+	db 5, 3, SLUDGE
+	db 6, 1, PSYCHIC_M
+	db 6, 2, MEGA_DRAIN
+	db 6, 3, THUNDER
 	db 0
 
+	
 	db LANCE, 1
-	db 1, 1, DRAGON_RAGE
-	db 2, 1, THUNDER_WAVE
-	db 2, 3, THUNDERBOLT
-	db 3, 1, BUBBLEBEAM
-	db 3, 2, WRAP
-	db 3, 3, ICE_BEAM
-	db 4, 1, WING_ATTACK
-	db 4, 2, SWIFT
-	db 4, 3, FLY
-	db 5, 1, BLIZZARD
-	db 5, 2, FIRE_BLAST
-	db 5, 3, THUNDER
-	db 0
-
-	db RIVAL3, 1
-	db 1, 3, EARTHQUAKE
-	db 2, 4, KINESIS
-	db 3, 4, LEECH_SEED
-	db 4, 1, ICE_BEAM
-	db 5, 1, CONFUSE_RAY
-	db 5, 4, FIRE_SPIN
-	db 6, 3, QUICK_ATTACK
-	db 0
-
-	db RIVAL3, 2
-	db 1, 3, EARTHQUAKE
-	db 2, 4, KINESIS
-	db 3, 4, LEECH_SEED
+	db 1, 1, FIRE_BLAST
+	db 1, 2, THUNDERBOLT
+	db 1, 3, SURF
+	db 1, 4, DRAGON_RAGE
+	db 2, 1, FLAMETHROWER
+	db 2, 2, EARTHQUAKE
+	db 2, 3, FLY
+	db 2, 4, DOUBLE_TEAM
+	db 3, 1, ICE_BEAM
+	db 3, 2, SURF
+	db 3, 3, HYDRO_PUMP
+	db 3, 4, SLAM
 	db 4, 1, THUNDERBOLT
-	db 5, 1, ICE_BEAM
-	db 6, 2, REFLECT
-	db 6, 3, QUICK_ATTACK
+	db 4, 2, LIGHT_SCREEN
+	db 4, 3, THUNDERPUNCH
+	db 4, 4, THUNDER_WAVE
+	db 5, 1, SKY_ATTACK
+	db 5, 2, ROCK_SLIDE
+	db 5, 3, HYPER_BEAM
+	db 5, 4, EARTHQUAKE
+	db 6, 1, FIRE_BLAST
+	db 6, 2, BLIZZARD
+	db 6, 3, THUNDER_WAVE
+	db 6, 4, HYPER_BEAM
 	db 0
 
-	db RIVAL3, 3
-	db 1, 3, EARTHQUAKE
-	db 2, 4, KINESIS
-	db 3, 4, LEECH_SEED
-	db 4, 1, CONFUSE_RAY
-	db 4, 4, FIRE_SPIN
-	db 5, 1, THUNDERBOLT
-	db 6, 1, AURORA_BEAM
-	db 6, 3, QUICK_ATTACK
-	db 0
+	db PROF_OAK, 1 
+    	db 1, 1, HYPER_BEAM
+    	db 1, 2, EARTHQUAKE
+    	db 1, 3, BLIZZARD
+    	db 1, 4, FIRE_BLAST
+    	db 2, 1, MEGA_DRAIN
+    	db 2, 2, SOFTBOILED
+    	db 2, 3, PSYCHIC_M
+    	db 2, 4, SOLARBEAM
+	db 3, 1, DIG
+    	db 3, 2, FLAMETHROWER
+    	db 3, 3, BODY_SLAM
+	db 3, 4, FIRE_BLAST
+    	db 4, 1, THUNDERBOLT
+    	db 4, 2, AMNESIA
+	db 4, 3, SELFDESTRUCT
+	db 4, 4, PSYCHIC_M
+	db 5, 1, DOUBLE_TEAM
+	db 5, 2, HYDRO_PUMP
+    	db 5, 3, THUNDERBOLT
+    	db 5, 3, FIRE_BLAST
+    	db 6, 1, ICE_BEAM
+    	db 6, 2, EARTHQUAKE
+    	db 6, 3, HORN_DRILL
+    	db 6, 4, SUBSTITUTE
+    	db 0
+	
 
 	db -1 ; end
