0b2fbbe06b238b1683240b91b91c8c6fa4d2fd00
CreamElDudJafar <jahern2398@gmail.com>
2025-01-15 11:15:31 -0600

    Can Now Teleport from Inside Buildings and Caves

--- engine/menus/start_sub_menus.asm
+++ engine/menus/start_sub_menus.asm
@@ -218,14 +218,14 @@ StartMenu_Pokemon::
 	call GBPalWhiteOutWithDelay3
 	jp .goBackToMap
 .teleport
-	call CheckIfInFlyMap
+;	call CheckIfInFlyMap
 	jr z, .canTeleport
-	ld a, [wWhichPokemon]
-	ld hl, wPartyMonNicks
-	call GetPartyMonName
-	ld hl, .cannotUseTeleportNowText
-	call PrintText
-	jp .loop
+;	ld a, [wWhichPokemon]
+;	ld hl, wPartyMonNicks
+;	call GetPartyMonName
+;	ld hl, .cannotUseTeleportNowText
+;	call PrintText
+;	jp .loop
 .canTeleport
 	ld hl, .warpToLastPokemonCenterText
 	call PrintText
