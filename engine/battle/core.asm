6785533ebc0c12efb7c6e91f73d59ae223eb9587
CreamElDudJafar <jahern2398@gmail.com>
2025-01-23 18:33:58 -0600

    Fix Substitute Can Make You Faint at 0HP Left

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -4320,7 +4320,7 @@ IgnoredOrdersText:
 GetDamageVarsForPlayerAttack:
 	xor a
 	ld hl, wDamage ; damage to eventually inflict, initialise to zero
-	ldi [hl], a
+	ld [hli], a
 	ld [hl], a
 	ld hl, wPlayerMovePower
 	ld a, [hli]
@@ -4626,8 +4626,8 @@ CalculateDamage:
 
 	xor a
 	ld hl, hDividend
-	ldi [hl], a
-	ldi [hl], a
+	ld [hli], a
+	ld [hli], a
 	ld [hl], a
 
 ; Multiply level by 2
@@ -4640,7 +4640,7 @@ CalculateDamage:
 	pop af
 .nc
 	inc hl
-	ldi [hl], a
+	ld [hli], a
 
 ; Divide by 5
 	ld a, 5
