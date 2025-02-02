b9e91cd89dcd438be1c741d2cb1945a187081f25
CreamElDudJafar <jahern2398@gmail.com>
2025-02-02 11:45:28 -0600

    Fixed Evoluton Bug Crash
    Game would crash upon evolution.
    
    Forgot to have a ret when jumping to the new file due to this being called.

--- data/pokemon/copy_base_stats.asm
+++ data/pokemon/copy_base_stats.asm
@@ -1,8 +1,8 @@
-CopyBaseStats::
-ld a, [wPokedexNum]
+CopyBaseStats:
+	ld a, [wPokedexNum]
 	dec a
 	ld hl, BaseStats
 	ld bc, BASE_DATA_SIZE
 	call AddNTimes
 	ld de, wMonHeader
-	rst _CopyData
+	jp CopyData
