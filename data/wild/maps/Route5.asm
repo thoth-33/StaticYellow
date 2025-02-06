7e42a761e9ac5a2ca99474fffdab47afe74da539
CreamElDudJafar <jahern2398@gmail.com>
2025-02-06 09:15:51 -0600

    Route 5 Wild Level Changes

--- data/wild/maps/Route5.asm
+++ data/wild/maps/Route5.asm
@@ -2,14 +2,14 @@ Route5WildMons:
 	def_grass_wildmons 15 ; encounter rate
 	db 15, PIDGEY
 	db 14, RATTATA
-	db  7, MEOWTH
+	db 13, MEOWTH
 	db 16, PIDGEY
 	db 16, ABRA
-	db 17, ABRA
+	db 15, ABRA
 	db 17, PIDGEOTTO
-	db  3, JIGGLYPUFF
-	db  5, JIGGLYPUFF
-	db  7, JIGGLYPUFF
+	db 12, JIGGLYPUFF
+	db 13, JIGGLYPUFF
+	db 13, JIGGLYPUFF
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
