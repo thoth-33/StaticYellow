f73beefbba5b167da6c60ed032a6a547b4c7954f
CreamElDudJafar <jahern2398@gmail.com>
2025-03-12 16:01:01 -0500

    Readded Removed Trades from Vanilla Yellow
    Matches Red and Blue Now.

--- scripts/VermilionTradeHouse.asm
+++ scripts/VermilionTradeHouse.asm
@@ -4,7 +4,15 @@ VermilionTradeHouse_Script:
 VermilionTradeHouse_TextPointers:
 	def_text_pointers
 	dw_const VermilionTradeHouseGentlemanText, TEXT_VERMILIONTRADEHOUSE_GENTLEMAN
+	dw_const VermilionTradeHouseLittleGirlText, TEXT_VERMILIONTRADEHOUSE_LITTLE_GIRL
 
 VermilionTradeHouseGentlemanText:
 	text_far TeachingHMsText
 	text_end
+
+VermilionTradeHouseLittleGirlText:
+	text_asm
+	ld a, TRADE_FOR_DUX
+	ld [wWhichTrade], a
+	predef DoInGameTradeDialogue
+	rst TextScriptEnd
