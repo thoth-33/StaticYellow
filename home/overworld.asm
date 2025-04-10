1f320ac20aa0cc5517aaf120e68008b6d59f6a69
CreamElDudJafar <jahern2398@gmail.com>
2025-04-10 10:51:11 -0500

    Remove Unused Blues House Constants/Unreferenced Function
    Got idea from Pret user.

--- home/overworld.asm
+++ home/overworld.asm
@@ -1960,8 +1960,8 @@ LoadMapHeader::
 	farcall MarkTownVisitedAndLoadMissableObjects
 	jr asm_0dbd
 
-Func_0db5:: ; XXX
-	farcall LoadMissableObjectData
+;Func_0db5:: ; XXX
+;	farcall LoadMissableObjectData
 asm_0dbd:
 	ld a, [wCurMapTileset]
 ;	ld [wUnusedCurMapTilesetCopy], a
