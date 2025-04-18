6bd12ce04adb41dfc43918cd470e5bc4e0ff042c
CreamElDudJafar <jahern2398@gmail.com>
2025-04-18 14:30:45 -0500

    Added 5th Jessie and James Fight
    In Mansion B1F

--- text/PokemonMansionB1F.asm
+++ text/PokemonMansionB1F.asm
@@ -1,3 +1,43 @@
+_PokemonMansionJessieJamesText1::
+	text "Stop right there!"
+	line "Not a step more!@"
+	text_end
+
+_PokemonMansionJessieJamesText2::
+	text "How dare you come"
+	line "down here and spy"
+	cont "on us!"
+
+	para "We are searching"
+	line "for a super rare"
+	cont "#MON for the"
+	cont "BOSS!"
+
+	para "We won't let him"
+	line "down again!"
+	
+	para "However, now we"
+	line "will finally get"
+	cont "our revenge on"
+	cont "you!"
+	done
+
+_PokemonMansionJessieJamesText3::
+	text "Oh no!"
+
+	para "Not again! We"
+	line "cannot show our"
+	cont "faces around here"
+	cont "anymore."
+	prompt
+
+_PokemonMansionJessieJamesText4::
+	text "Looks like TEAM"
+	line "ROCKET's blasting"
+	cont "off one last time"
+	cont "again!@"
+	text_end
+
 _PokemonMansionB1FBurglarBattleText::
 	text "Uh-oh. Where am"
 	line "I now?"
