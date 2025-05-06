2943ce5ddad22119c2d6c2d3420e20f1d994bcf3
CreamElDudJafar <jahern2398@gmail.com>
2025-05-05 21:21:22 -0500

    Edit Daisy Town Map Text
    Tells player how to use Town Map now.

--- text/BluesHouse1F.asm
+++ text/BluesHouse1F.asm
@@ -11,17 +11,24 @@ _BluesHouseDaisyOfferMapText::
 	cont "help you!"
 	prompt
 
+
 _GotMapText::
 	text "<PLAYER> got a"
-	line "TOWN MAP!"
-	done
-
-;_GotMapText::
-;	text "<PLAYER> got a"
 ;	line "@"
 ;	text_ram wStringBuffer
 ;	text "!@"
-;	text_end
+	line "TOWN MAP!@"
+	text_end
+
+_MapHelpText::
+	text "You can press"
+	line "SELECT when in"
+	cont "your POKéDEX to"
+	cont "use the TOWN MAP."
+
+	para "It is also in the"
+	line "START MENU now."
+	done
 
 _BluesHouseDaisyBagFullText::
 	text "You have too much"
