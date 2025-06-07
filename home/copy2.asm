cd8cb47b0fa95b9bb6ba703338612716c2a878d6
CreamElDudJafar <jahern2398@gmail.com>
2025-06-07 16:12:47 -0500

    Update Copy2
    Removed this from last commit

--- home/copy2.asm
+++ home/copy2.asm
@@ -1,18 +1,3 @@
-FarCopyData2::
-; Identical to FarCopyData, but uses hROMBankTemp
-; as temp space instead of wBuffer.
-	ldh [hROMBankTemp], a
-	ldh a, [hLoadedROMBank]
-	push af
-	ldh a, [hROMBankTemp]
-	ldh [hLoadedROMBank], a
-	ld [MBC1RomBank], a
-	call CopyData
-	pop af
-	ldh [hLoadedROMBank], a
-	ld [MBC1RomBank], a
-	ret
-
 IsTilePassable::
 ; sets carry if tile is passable, resets carry otherwise
 	homecall_sf _IsTilePassable
