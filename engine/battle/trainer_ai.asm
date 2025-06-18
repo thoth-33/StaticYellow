6fd242d4edf2417d0151aeccbd5d2c9c7f870803
CreamElDudJafar <jahern2398@gmail.com>
2025-06-18 09:12:38 -0500

    Fix Stats Not Being Reset After Curing Paralyze or Burn
    Credit: Jojobear/ShinPokeRed

--- engine/battle/trainer_ai.asm
+++ engine/battle/trainer_ai.asm
@@ -1051,7 +1051,7 @@ AICureStatus:	;shinpokerednote: CHANGED: modified to be more robust and also und
 	push af
 	ld a, $01 	;forcibly set it to the AI's turn
 	ldh [hWhoseTurn], a
-	call UndoBurnParStats	;undo brn/par stat changes
+	farcall UndoBurnParStats	;undo brn/par stat changes
 	pop af
 	ldh [hWhoseTurn], a
 	xor a
@@ -1203,37 +1203,4 @@ StoreBattleMonTypes:
 	ld a, [hl]                 ; c = type 2 of player's pokemon
 	ld [wAITargetMonType2], a
 	pop hl
-	ret
-
-; Used by the pureRGB AI
-;shinpokerednote: ADDED: doubles attack if burned or quadruples speed if paralyzed.
-;It's meant to be run right before healing paralysis or burn so as to 
-;undo the stat changes.
-UndoBurnParStats:
-	ld hl, wBattleMonStatus
-	ld de, wPlayerStatsToDouble
-	ldh a, [hWhoseTurn]
-	and a
-	jr z, .checkburn
-	ld hl, wEnemyMonStatus
-	ld de, wEnemyStatsToDouble
-.checkburn
-	ld a, [hl]		;load statuses
-	and 1 << BRN	;test for burn 
-	jr z, .checkpar
-	ld a, $01
-	ld [de], a	;set attack to be doubled to undo the stat change of BRN
-	call DoubleSelectedStats
-	jr .return
-.checkpar
-	ld a, [hl]		;load statuses
-	and 1 << PAR	;test for paralyze 
-	jr z, .return
-	ld a, $04
-	ld [de], a	;set speed to be doubled (done twice) to undo the stat change of BRN
-	call DoubleSelectedStats
-	call DoubleSelectedStats
-.return
-	xor a
-	ld [de], a	;reset the stat change bits
 	ret
